aeaa605c0d5f7e2766bf159cc85066bc766f825b
Iain <Iain@DESKTOP-BKE6KM9>
2019-04-02 20:56:48 -0500

    - Playing around with `typealias` - Added a Deck Factory

--- src/main/kotlin/com/xplore/cards/domain/Deck.kt
+++ src/main/kotlin/com/xplore/cards/domain/Deck.kt
@@ -1,15 +1,32 @@
 package com.xplore.cards.domain
 
-class Deck {
-    val cards: List<Card>
+typealias Deck = List<Card>
 
-    init {
-        val clubs = (1..13).map { Card(it, Suit.CLUBS) }
-        val diamonds = (1..13).map { Card(it, Suit.DIAMONDS) }
-        val hearts = (1..13).map { Card(it, Suit.HEARTS) }
-        val spades = (1..13).map { Card(it, Suit.SPADES) }
-        val jokers = (1..2).map { Card(14, Suit.JOKER) }
+enum class DeckType {
+    STANDARD
+}
 
-        cards = clubs + diamonds + hearts + spades + jokers
+interface DeckFactory {
+    fun create(deckType: DeckType) : Deck
+}
+
+class StandardDeckFactory {
+
+    companion object : DeckFactory {
+        override fun create(deckType: DeckType): Deck =
+                when(deckType) {
+                    DeckType.STANDARD -> standardDeck()
+
+        }
+
+        private fun standardDeck(): Deck {
+            val clubs = (1..13).map { Card(it, Suit.CLUBS) }
+            val diamonds = (1..13).map { Card(it, Suit.DIAMONDS) }
+            val hearts = (1..13).map { Card(it, Suit.HEARTS) }
+            val spades = (1..13).map { Card(it, Suit.SPADES) }
+            val jokers = (1..2).map { Card(14, Suit.JOKER) }
+            return clubs + diamonds + hearts + spades + jokers
+        }
+
     }
 }
--- src/test/kotlin/com/xplore/cards/domain/DeckTest.kt
+++ src/test/kotlin/com/xplore/cards/domain/DeckTest.kt
@@ -6,32 +6,32 @@ import org.spekframework.spek2.Spek
 object DeckTest : Spek({
     group("Initialization Tests") {
         test(" A Standard Deck can be created successfully") {
-            val deck = Deck()
-            assertThat(deck.cards.size).isEqualTo(54)
+            val deck = StandardDeckFactory.create(DeckType.STANDARD)
+            assertThat(deck.size).isEqualTo(54)
         }
         test("A Standard Deck contains the correct number of Clubs") {
-            val deck = Deck()
-            val clubs = deck.cards.filter { it.suit == Suit.CLUBS }
+            val deck = StandardDeckFactory.create(DeckType.STANDARD)
+            val clubs = deck.filter { it.suit == Suit.CLUBS }
             assertThat(clubs.size).isEqualTo(13)
         }
         test("A Standard Deck contains the correct number of Diamonds") {
-            val deck = Deck()
-            val diamonds = deck.cards.filter { it.suit == Suit.DIAMONDS }
+            val deck = StandardDeckFactory.create(DeckType.STANDARD)
+            val diamonds = deck.filter { it.suit == Suit.DIAMONDS }
             assertThat(diamonds.size).isEqualTo(13)
         }
         test("A Standard Deck contains the correct number of Hearts") {
-            val deck = Deck()
-            val hearts = deck.cards.filter { it.suit == Suit.HEARTS }
+            val deck = StandardDeckFactory.create(DeckType.STANDARD)
+            val hearts = deck.filter { it.suit == Suit.HEARTS }
             assertThat(hearts.size).isEqualTo(13)
         }
         test("A Standard Deck contains the correct number of Spades") {
-            val deck = Deck()
-            val spades = deck.cards.filter { it.suit == Suit.SPADES }
+            val deck = StandardDeckFactory.create(DeckType.STANDARD)
+            val spades = deck.filter { it.suit == Suit.SPADES }
             assertThat(spades.size).isEqualTo(13)
         }
         test("A Standard Deck contains the correct number of Jokers") {
-            val deck = Deck()
-            val jokers = deck.cards.filter { it.suit == Suit.JOKER }
+            val deck = StandardDeckFactory.create(DeckType.STANDARD)
+            val jokers = deck.filter { it.suit == Suit.JOKER }
             assertThat(jokers.size).isEqualTo(2)
         }
     }
